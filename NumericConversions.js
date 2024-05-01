21c5944f729254209d6cb4b713e3dd2e65ed600f
Mark Grimmond <lowkeymxvk@gmail.com>
2024-05-01 13:49:09 -0400

    clean up git

---
+++ NumericConversions.js
@@ -0,0 +1,21 @@
+"use strict"
+// Declare variables 
+var x =  "101"
+var y = parseInt(x, 2);
+
+// Results
+console.log(y);
+
+//
+var x =  "101"
+var y = parseFloat(x, 2);
+
+// Results
+console.log(y);
+
+
+var x =  "55"
+var y = parseInt(x, 2);
+
+// Results
+console.log(y);
---
+++ demo2.7.js
@@ -0,0 +1,10 @@
+"use strict";
+
+var x ="123";
+var y = "456";
+
+var xInt = parseInt(x);
+var yInt = parseInt(y);
+
+var z = x + y;
+console.log(z);
